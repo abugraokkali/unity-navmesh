45974e8986552eefff1544235606e037e25c9317
abugraokkali <abugraokkali@outlook.com>
2022-08-01 23:53:06 +0300

    Added movement by using mouse click

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -7,16 +7,23 @@ public class MoveTo : MonoBehaviour
 {
 
     public Transform goal;
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();   
+        agent = GetComponent<NavMeshAgent>();   
         agent.destination = goal.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                agent.destination = hit.point;
+            }
+        }
     }
 }
