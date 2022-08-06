5cf3fddb0501b7de4d2530af85ad0efa17aea0cc
abugraokkali <abugraokkali@outlook.com>
2022-08-07 00:53:42 +0300

    Added mouse click character movements

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -18,12 +18,15 @@ public class MoveTo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
             RaycastHit hit;
             
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
                 agent.destination = hit.point;
             }
+        }
+        else {
+            agent.destination = goal.position;
         }
     }
 }
